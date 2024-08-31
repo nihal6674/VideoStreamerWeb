9ce627b86350e4790de7aa5be704a5db53419dce
nihal6674 <nmohammaedali@gmail.com>
2024-08-31 09:19:00 +0530

    just to keep up to the line commit

---
+++ src/db/dbConnect.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DBNAME } from "../constants.js";
+
+const connectDB=async ()=>{
+    try {
+        const connectionInstance=await mongoose.connect("mongodb+srv://nihal:Nihal%40123@cluster0.ucbrc.mongodb.net/")
+        console.log(`\n Database connected !! ConnectionInstance is ${connectionInstance.connection.host} `)
+    } catch (error) {
+
+        console.log("MondgoDB connection error",error)
+        process.exit(1)
+        
+    }
+}
+
+
+export default connectDB
